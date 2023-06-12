3d15310c94201ed662725776db6da8c92241b29b
Guilherme Martins <111918966+GMartin-s@users.noreply.github.com>
2023-06-11 21:09:29 -0300

    Add files via upload

---
+++ src/js/login.js
@@ -0,0 +1,33 @@
+
+function validarFormulario() {
+    const login = document.getElementById("email").value;
+    const senha = document.getElementById("password").value;
+
+    // Verifica se o campo de login está vazio
+    if (login.trim() === "") {
+      alert("Por favor, insira seu e-mail.");
+      return false;
+    }
+
+    // Verifica se o e-mail é válido
+    if (!validarEmail(login)) {
+      alert("Por favor, insira um e-mail válido.");
+      return false;
+    }
+
+    // Verifica se o campo de senha está vazio
+    if (senha.trim() === "") {
+      alert("Por favor, insira sua senha.");
+      return false;
+    }
+
+    // Se todas as validações passarem, redireciona para o link na mesma janela
+    window.location.href = "./home-page.html"; 
+    // Retorna false para evitar o envio do formulário
+    return false;
+  }
+
+  function validarEmail(email) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  }
